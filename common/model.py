4e00753e310229a984b83d91be6372d36d8ab75b
nikxtaco <nikitamenon2510@gmail.com>
2023-11-12 22:49:04 +0530

    Code cleanup

--- common/model.py
+++ common/model.py
@@ -8,61 +8,6 @@ class Flatten(nn.Module):
         return torch.flatten(x, start_dim=1) # Changed 1 to 0
 
 
-# class MlpModel(nn.Module):
-#     def __init__(self,
-#                  input_dims=4,
-#                  hidden_dims=[64, 64],
-#                  **kwargs):
-#         """
-#         input_dim:     (int)  number of the input dimensions
-#         hidden_dims:   (list) list of the dimensions for the hidden layers
-#         use_batchnorm: (bool) whether to use batchnorm
-#         """
-#         super(MlpModel, self).__init__()
-
-#         # Hidden layers
-#         hidden_dims = [input_dims] + hidden_dims
-#         layers = []
-#         for i in range(len(hidden_dims) - 1):
-#             in_features = hidden_dims[i]
-#             out_features = hidden_dims[i + 1]
-#             layers.append(nn.Linear(in_features, out_features))
-#             layers.append(nn.ReLU())
-#         self.layers = nn.Sequential(*layers)
-#         self.output_dim = hidden_dims[-1]
-#         self.apply(orthogonal_init)
-
-#     def forward(self, x):
-#         for layer in self.layers:
-#             x = layer(x)
-#         return x
-
-
-# class NatureModel(nn.Module):
-#     def __init__(self,
-#                  in_channels,
-#                  **kwargs):
-#         """
-#         input_shape:  (tuple) tuple of the input dimension shape (channel, height, width)
-#         filters:       (list) list of the tuples consists of (number of channels, kernel size, and strides)
-#         use_batchnorm: (bool) whether to use batchnorm
-#         """
-#         super(NatureModel, self).__init__()
-#         self.layers = nn.Sequential(
-#             nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4), nn.ReLU(),
-#             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2), nn.ReLU(),
-#             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1), nn.ReLU(),
-#             Flatten(),
-#             nn.Linear(in_features=16*8*8, out_features=512), nn.ReLU() # changed 64*7*7 to 16*8*8
-#         )
-#         self.output_dim = 512
-#         self.apply(orthogonal_init)
-
-#     def forward(self, x):
-#         x = self.layers(x)
-#         return x
-
-
 class ResidualBlock(nn.Module):
     def __init__(self,
                  in_channels):
